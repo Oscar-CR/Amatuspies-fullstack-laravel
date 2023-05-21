ffe5f8d037fb63d68aa7c97afa39d7c11e3b06fd
Oscar-CR <krz.oficial@gmail.com>
2023-05-21 02:08:53 -0600

    fix texto de pagina y borrado de link en el nombre de usuario de la nav

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -3,8 +3,7 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="{{ route('home') }}"
-            target="_blank">
+        <a class="navbar-brand m-0" href="#">
             <span class="ms-1 font-weight-bold ">¡Hola {{auth()->user()->name}}!</span>
         </a>
     </div>
